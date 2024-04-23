c7a6d03a326f8d85e1a5b9be158c1e547bf40887
RusskiyPythonist <ronon.obvinitely56@gmail.com>
2024-04-23 17:02:15 +0500

    - Добавлены комментарии к коду - Изменены названия аргументов методов - Все методы перенесены в отдельный файл "ElementsUtility.cs"

---
+++ RadioactiveDecayCalculating/Modules/ElementsUtility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Bluegrams.Periodica.Data;
+
+namespace RadioactiveDecayCalculating.Modules
+{
+    internal class ElementsUtility
+    {
+        static public bool ElementIsExist(PeriodicTable periodicTable, string elementSymbol) // Существует ли элемент в таблице элементов
+        {
+            bool isAtTable;
+
+            try { isAtTable = periodicTable[elementSymbol].Symbol != null; }
+
+            catch { isAtTable = false; }
+
+            return isAtTable;
+        }
+
+        static public Element GetElementBySymbol(PeriodicTable periodicTable, string elementSymbol) // Получение элемента по его символу
+        {
+            return periodicTable[elementSymbol];
+        }
+
+        static public Element GetElementByAtomicNumber(PeriodicTable periodicTable, int atomicNumber) // Получение элемента по его атомному номеру
+        {
+            return periodicTable[atomicNumber];
+        }
+
+        static public Element GetElementByName(PeriodicTable periodicTable, string elementSymbol)
+        {
+            return periodicTable[elementSymbol];
+        }
+
+        static public bool IsIsotope(int atomicMass1, int atomicMass2) // Является ли элемент изотопом
+        {
+            return atomicMass1 == atomicMass2 ? false : true;
+        }
+    }
+}
--- RadioactiveDecayCalculating/Program.cs
+++ RadioactiveDecayCalculating/Program.cs
@@ -1,46 +1,25 @@
 ﻿using System;
 using Bluegrams.Periodica.Data;
+using ElementsUtility = RadioactiveDecayCalculating.Modules.ElementsUtility;
 
-namespace RadioactiveDecayCalculating // Note: actual namespace depends on the project name.
+namespace RadioactiveDecayCalculating
 {
     internal class Program
     {
-        static private bool ElementIsExist(PeriodicTable periodicTable, string elementName)
-        {
-            bool isAtTable;
-
-            try { isAtTable = periodicTable[elementName].Symbol != null; }
-
-            catch { isAtTable = false; }
-
-            return isAtTable;
-        }
-
-        static private Element GetElementByName(PeriodicTable periodicTable, string elementName)
-        {
-            return periodicTable[elementName];
-        }
-
-        static private Element GetElementByAtomicNumber(PeriodicTable periodicTable, int atomicNumber)
-        {
-            return periodicTable[atomicNumber];
-        }
-		
-		static private bool IsIsotope(int atomicMass1, int atomicMass2) { return atomicMass1 == atomicMass2 ? false : true; }
 
         static void Main(string[] args)
         {
-            var periodicTable = PeriodicTable.Load();
+            var periodicTable = PeriodicTable.Load(); // Загрузка таблицы элементов
             
 			Console.WriteLine("Введите химический элемент");
 			Console.Write(">> ");
-            string elementName = Convert.ToString(Console.ReadLine());
+            string elementSymbol = Convert.ToString(Console.ReadLine()); // Ввод символа элемента с клавиатуры
 
-            if (elementName != null && elementName.Length <= 2 && ElementIsExist(periodicTable, elementName) == true)
+            if (elementSymbol != null && ElementsUtility.ElementIsExist(periodicTable, elementSymbol) == true)
             {
-                Element elementToDecay = GetElementByName(periodicTable, elementName);
-                int elementAtomicMass = Convert.ToInt32(Math.Ceiling(elementToDecay.AtomicMass));
-                int elementAtomicNumber = elementToDecay.AtomicNumber;
+                Element elementToDecay = ElementsUtility.GetElementBySymbol(periodicTable, elementSymbol); // Получаем элемент по символу
+                int elementAtomicMass = Convert.ToInt32(Math.Ceiling(elementToDecay.AtomicMass)); // Атомная масса вводимого элемента
+                int elementAtomicNumber = elementToDecay.AtomicNumber; // Атомный номер вводимого элемента
 
                 Console.WriteLine("Введите реакцию распада");
                 Console.WriteLine("1 - Альфа-распад");
@@ -52,16 +31,18 @@ namespace RadioactiveDecayCalculating // Note: actual namespace depends on the p
 
                 if (input == 1)
                 {
-                    Element helium = GetElementByName(periodicTable, "He");
-                    int heliumAtomicMass = Convert.ToInt32(helium.AtomicMass);
-                    int heliumAtomicNumber = helium.AtomicNumber;
+                    Element helium = ElementsUtility.GetElementByName(periodicTable, "He"); // Получение информации об элементе "Гелий"
+                    int heliumAtomicMass = Convert.ToInt32(helium.AtomicMass); // Атомная масса гелия
+                    int heliumAtomicNumber = helium.AtomicNumber; // Атомный номер гелия
 
-                    int finalElementAtomicMass = elementAtomicMass - heliumAtomicMass;
-                    int finalElementAtomicNumber = elementAtomicNumber - heliumAtomicNumber;
-                    Element finalElement = GetElementByAtomicNumber(periodicTable, finalElementAtomicNumber);
+                    int finalElementAtomicMass = elementAtomicMass - heliumAtomicMass; // Вычисление атомной массы получившегося элемента
+                    int finalElementAtomicNumber = elementAtomicNumber - heliumAtomicNumber; // Вычисление атомного номера получившегося элемента
+                    Element finalElement = ElementsUtility.GetElementByAtomicNumber(periodicTable, finalElementAtomicNumber); // Находим получившийся элемент по атомному номеру
 
-                    int tableAtomicMass = Convert.ToInt32(finalElement.AtomicMass);
-                    bool elementIsIsotope = IsIsotope(tableAtomicMass, finalElementAtomicMass);
+                    int tableAtomicMass = Convert.ToInt32(finalElement.AtomicMass); // Получаем табличное значение атомной массы элемента
+                    bool elementIsIsotope = ElementsUtility.IsIsotope(tableAtomicMass, finalElementAtomicMass); // Проверка на изотоп
+
+                    // Вывод информации в консоль
 
                     Console.WriteLine($"{elementAtomicMass}     {finalElementAtomicMass}    {heliumAtomicMass}");
                     Console.WriteLine($" {elementToDecay.Symbol} -->  {finalElement.Symbol}  +  {helium.Symbol}");
@@ -70,15 +51,17 @@ namespace RadioactiveDecayCalculating // Note: actual namespace depends on the p
                     Console.WriteLine($"Является изотопом: {elementIsIsotope}");
                 } else if (input == 2)
                 {
-                    const int electronAtomicMass = 0;
-                    const int electronAtomicNumber = -1;
+                    const int electronAtomicMass = 0; // Атомная масса электрона
+                    const int electronAtomicNumber = -1; // Атомный номер электрона (так как отрицательный заряд)
 
-                    int finalElementAtomicMass = elementAtomicMass - electronAtomicMass;
-                    int finalElementAtomicNumber = elementAtomicNumber - electronAtomicNumber;
-                    Element finalElement = GetElementByAtomicNumber(periodicTable, finalElementAtomicNumber);
+                    int finalElementAtomicMass = elementAtomicMass - electronAtomicMass; // Вычисление атомной массы получившегося элемента
+                    int finalElementAtomicNumber = elementAtomicNumber - electronAtomicNumber; // Вычисление атомного номера получившегося элемента
+                    Element finalElement = ElementsUtility.GetElementByAtomicNumber(periodicTable, finalElementAtomicNumber); // Находим получившийся элемент по атомному номеру
 
-                    int tableAtomicMass = Convert.ToInt32(finalElement.AtomicMass);
-                    bool elementIsIsotope = IsIsotope(tableAtomicMass, elementAtomicMass);
+                    int tableAtomicMass = Convert.ToInt32(finalElement.AtomicMass); // Получаем табличное значение атомной массы элемента
+                    bool elementIsIsotope = ElementsUtility.IsIsotope(tableAtomicMass, elementAtomicMass); // Проверка на изотоп
+
+                    // Вывод информации в консоль
 
                     Console.WriteLine($"{elementAtomicMass}      {finalElementAtomicMass}     {electronAtomicMass}");
                     Console.WriteLine($" {elementToDecay.Symbol} -->  {finalElement.Symbol}  +   e");
